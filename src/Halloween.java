27999362c32e27c7b104a95fba7985a3c1eb31ba
Sienna Shen <sienna_shen@horacemann.org>
2025-10-15 19:02:21 -0400

    Updated Halloween class so that multiple ghosts can be printed on each row

--- src/Halloween.java
+++ src/Halloween.java
@@ -1,54 +1,77 @@
 import java.util.Random;
 
 public class Halloween {
+
+    static final Random rand = new Random();
+
     static final String squashed_ghost1 = "   .---.     '     '   | o   o |  |   ~   | <|       |> |       \\_  \\       /    -----' ";
     static final String squashed_ghost2 = "   .---.     '     '   | @   @ |  |   U   | <|       |> |       \\_  \\       /    -----' ";
-    static final Random rand = new Random();
-    public static void main(String[] args) throws Exception {
-        char[][] ghost1 = makeObject(squashed_ghost1, 8, 11);
-        char[][] ghost2 = makeObject(squashed_ghost2, 8, 11);
+    static char[][] painting = new char[8][120]; //canvas who's rows we will iterate through
 
+    public static void main(String[] args) throws Exception {
+        emptyCanvas();
+        
         while (true){
 
-            double roll = rand.nextDouble(0,1);
-            if (roll < 0.5){
-                printObject(ghost1, rand.nextInt(0,109));
-            }else{
-                printObject(ghost2, rand.nextInt(0,109));
+            double roll = rand.nextDouble(0,3); //Add no ghost, ghost1, or ghost2 to canvas
+            if(roll < 1){
+                paintObject(squashed_ghost1, rand.nextInt(0,110), 8, 11);
+            } else if (roll < 2){
+                paintObject(squashed_ghost2, rand.nextInt(0,110), 8, 11);
             }
-            
-            Thread.sleep(200);
+
+            printRow(0);
+            shiftUp();
+
+            Thread.sleep(100);
+
+        }
+
+    }
+
+    static void emptyCanvas(){ //Fills canvas with empty space
+        for(int i = 0; i < 8; i ++){
+            for(int j = 0; j < 120; j++){
+                painting[i][j] = ' ';
+            }
         }
     }
 
-    static char[][] makeObject(String characters, int n_rows, int n_cols){ //make 2D array of static art given its squashed version and dimensions
+    static void paintObject(String characters, int col, int n_rows, int n_cols){ //Adds characters onto canvas
         char[] squashed_object = characters.toCharArray(); //convert string to array
-        char[][] object = new char[n_rows][n_cols]; //declare & initialize array that will be returned
 
-        int k = 0; //index of squashed_object
-        for (int i = 0; i < n_rows; i++){ //fill the "object" array with the characters from squashed_object
-            for(int j = 0; j < n_cols; j++){
-                object[i][j] = squashed_object[k];
+        int k = 0; //Add characters to canvas given its dimensions (n_rows X n_cols) and column number of the leftmost side.
+        for (int i = 0; i < n_rows; i++){ 
+            for(int j = col; j < col + n_cols; j++){
+                painting[i][j] = squashed_object[k];
                 k++;
             }
         }
 
-        return object;
     }
 
-    static void printObject(char[][] object, int shift){ //print out static art starting at a specific column
-        for(int i = 0; i < object.length; i++){
-
-            for(int k = 0; k < shift; k++){//shift row (shift) spaces to the right
-                System.out.print(' ');
-            }
-    
-            for(int j = 0; j< object[0].length; j++){ //print entire row
-                System.out.print(object[i][j]);
-            }
-            
-            System.out.println();
-
+    static void printRow(int index){ 
+        for (int col = 0; col < 120; col++){
+            System.out.print(painting[index][col]);
         }
+
+        System.out.println();
+    }
+
+    static void shiftUp() throws InterruptedException{ //Shift every row up in the canvas
+        for(int row = 0; row < 7; row++){
+            painting[row] = painting[row+1];
+        }
+
+        painting[7] = emptyRow();
+    }
+
+    static char[] emptyRow() { //Create an empty row (for the bottom-most row of canvas after a shift)
+        char[] row = new char[120];
+        for (int i = 0; i < 120; i++) {
+            row[i] = ' ';
+        }
+    
+        return row;
     }
 }
